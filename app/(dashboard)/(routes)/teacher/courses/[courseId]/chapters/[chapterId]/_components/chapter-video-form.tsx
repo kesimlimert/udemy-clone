4e1e5d02a8c2d2075bd763da4bab354d5b20346f
kesimlimert <mert_cod@hotmail.com>
2024-10-14 02:58:11 +0300

    add video feature for chapter

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Pencil, PlusCircle, Video } from "lucide-react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Chapter, MuxData } from "@prisma/client";
+import Image from "next/image";
+import { FileUpload } from "@/components/file-upload";
+import MuxPlayer from "@mux/mux-player-react";
+
+interface ChapterVideoFormProps {
+  initialData: Chapter & { muxData?: MuxData | null };
+  courseId: string;
+  chapterId: string;
+}
+
+const formSchema = z.object({
+  videoUrl: z.string().min(1),
+});
+
+export function ChapterVideoForm({
+  initialData,
+  courseId,
+  chapterId,
+}: ChapterVideoFormProps) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((currentValue) => !currentValue);
+
+  const router = useRouter();
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      console.log("values", values);
+      await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}`, values);
+      toast.success("Chapter updated");
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    }
+  };
+
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Course video
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing && (
+            <>Cancel</>
+          )}
+          {!isEditing && !initialData.videoUrl && (
+            <>
+              <PlusCircle className="w-4 h-4 mr-2" />
+              Add a video
+            </>
+          )}
+          {!isEditing && initialData.videoUrl && (
+            <>
+              <Pencil className="w-4 h-4 mr-2" />
+              Edit video
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing && (
+        !initialData.videoUrl ? (
+          <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+            <Video className="w-10 h-10 text-slate-500" />
+          </div>
+        ) : (
+          <div className="relative aspect-video mt-2">
+            <MuxPlayer
+              playbackId={initialData?.muxData?.playbackId || ""}
+            />
+          </div>
+        )
+      )}
+      {isEditing && (
+        <div>
+          <FileUpload
+            endpoint="chapterVideo"
+            onChange={(url) => {
+              if (url) {
+                onSubmit({ videoUrl: url });
+              }
+            }}
+          />
+          <div className="text-xs text-muted-foreground mt-4">
+            Upload the video for this chapter
+          </div>
+        </div>
+      )}
+      {initialData.videoUrl && !isEditing && (
+        <div className="text-xs text-muted-foreground mt-2">
+          Videos can take a few minutes to process. Refresh the page if you're
+          not seeing the video.
+        </div>
+      )}
+    </div>
+  );
+}
--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,46 +1,80 @@
+import Mux from "@mux/mux-node";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+const mux = new Mux({
+  tokenId: process.env.MUX_TOKEN_ID!,
+  tokenSecret: process.env.MUX_TOKEN_SECRET!,
+});
+
 export async function PATCH(
-	req: Request,
-	{ params }: { params: { courseId: string; chapterId: string } }
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
 ) {
-	try {
-		const { userId } = auth();
-		const { isPublished, ...values } = await req.json();
+  try {
+    const { userId } = auth();
+    const { isPublished, ...values } = await req.json();
 
-		if (!userId) {
-			return new NextResponse("Unauthorized", { status: 401 });
-		}
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
 
-		const ownCourse = await db.course.findUnique({
-			where: {
-				id: params.courseId,
-				userId,
-			},
-		});
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
 
-		if (!ownCourse) {
-			return new NextResponse("Unauthorized", { status: 401 });
-		}
+    if (!ownCourse) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
 
-		const chapter = await db.chapter.update({
-			where: {
-				id: params.chapterId,
-				courseId: params.courseId,
-			},
-			data: {
-				...values,
-			},
-		});
+    const chapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        ...values,
+      },
+    });
 
-		// TODO: Handle video upload
+    if (values.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId: params.chapterId,
+        },
+      });
 
-		return NextResponse.json(chapter);
+      if (existingMuxData) {
+        await mux.video.assets.delete(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: {
+            id: existingMuxData.id,
+          },
+        });
+      }
 
-	} catch (error) {
-		console.log("[COURSES_CHAPTER_ID]", error);
-		return new NextResponse("Internal Error", { status: 500 });
-	}
+      const asset = await mux.video.assets.create({
+        input: values.videoUrl,
+        playback_policy: ["public"],
+        test: false,
+      });
+
+      await db.muxData.create({
+        data: {
+          chapterId: params.chapterId,
+          assetId: asset.id,
+          playbackId: asset.playback_ids?.[0]?.id,
+        },
+      });
+    }
+
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.log("[COURSES_CHAPTER_ID]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
